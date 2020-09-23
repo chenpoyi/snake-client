ef7d3e3e4484a7323a5db5bfa7ae03bc195fa5db
Paul Chen <paulchen647@gmail.com>
2020-09-23 21:13:24 +0000

    Added constants.js and key_mapping

---
+++ constants.js
@@ -0,0 +1,18 @@
+const IP = '135.23.222.131';
+const PORT = 50542;
+const KEY_MAPPING = {
+  w: 'Move: up',
+  a: 'Move: left',
+  s: 'Move: down',
+  d: 'Move: right',
+  f: 'Say: gg',
+  g: 'Say: nice!'
+}
+
+
+
+module.exports = {
+  IP,
+  PORT,
+  KEY_MAPPING
+};
--- input.js
+++ input.js
@@ -1,5 +1,5 @@
 const { stderr } = require("process");
-
+const {KEY_MAPPING} = require("./constants")
 let connection;
 
 
@@ -19,22 +19,25 @@ const setupInput = function(conn) {
 const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
-  } else if (data === 'w') {
-    connection.write('Move: up');
-  } else if (data === 'a') {
-    connection.write('Move: left');
-  } else if (data === 's') {
-    connection.write('Move: down');
-  } else if (data === 'd') {
-    connection.write('Move: right');
-  } else if (data === 'g') {
-    connection.write('Say: gg');
-  } else if (data === 'f') {
-    connection.write('Say: nice!');
+  } else {
+    connection.write(KEY_MAPPING[data]);
   }
+  
 };
 
 module.exports = {
   setupInput,
   handleUserInput
 };
+// if (data === 'w') {
+//   connection.write('Move: up');
+// } else if (data === 'a') {
+//   connection.write('Move: left');
+// } else if (data === 's') {
+//   connection.write('Move: down');
+// } else if (data === 'd') {
+//   connection.write('Move: right');
+// } else if (data === 'g') {
+//   connection.write('Say: gg');
+// } else if (data === 'f') {
+//   connection.write('Say: nice!');
